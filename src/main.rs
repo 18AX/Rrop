4ea9b17099e353fe19acc223c6d0c778423a5b4f
Alex Lev <levzx18@gmail.com>
2022-08-05 16:59:20 +0200

    parse the binary name from the arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Invalid args {}", err);
+        process::exit(1);
+    });
+
+    println!("binary: {}", config.binary_path);
+}
+
+struct Config
+{
+    binary_path: String
+}
+
+impl Config
+{
+    fn new(args: &[String]) -> Result<Config, &'static str>
+    {
+        if args.len() < 2
+        {
+            return Err("Not enough arguments");
+        }
+
+        let binary_path = args[1].clone();
+        Ok(Config{binary_path})
+    }
 }
